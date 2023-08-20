ce2cc0a4115535dbdd574c552889dac5bbed5817
pvilers <pvilers@hotmail.com>
2023-08-20 13:14:05 +0200

    1.0

---
+++ src/Article.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let article;
+</script>
+
+<div class="article">
+  <h2>{article.titre}</h2>
+  <img src={article.image} alt={article.titre} />
+  <p>{article.texte}</p>
+</div>
+
+<style>
+  .article {
+    margin: 5rem;
+  }
+  img {
+    width: 100%;
+    margin: 1rem auto;
+  }
+</style>
---
+++ src/Articles.svelte
@@ -0,0 +1,42 @@
+<script>
+import Article from "./Article.svelte";
+  let articles = [
+    {
+      image: "img/c21.jpg",
+      titre: "CENTURY 21",
+      texte:
+        "REALISATION DU SITE INTERNET DE CENTURY 21 AVANTAGE AVEC ACTIVIMMO",
+    },
+    
+    {
+      image: "img/sainte-elisabeth.jpg",
+      titre: "SAINTE-ELISABETH",
+      texte:
+        "RÉALISATION DU SITE INTERNET DE L’ÉCOLE SAINTE-ELISABETH D’ARCHENNES",
+    },
+    {
+      image: "img/terblock.jpg",
+      titre: "TERBLOCK",
+      texte:
+        "CRÉATION IDENTITÉ GRAPHIQUE ET SITE POUR LE CHÂTEAU DE TERBLOCK"
+    },
+    {
+      image: "img/laure.jpg",
+      titre: "LAURE",
+      texte:
+        "CRÉATION D’UN FLYER POUR LE SALON DE TOILETTAGE LAURE"
+    }
+  ];
+</script>
+<h1>Succes stories</h1>
+<div>
+{#each articles as article}
+<Article {article}/>
+{/each}
+</div>
+<style>
+  div {
+    margin-top : 10rem;
+    margin-bottom : 10rem;
+  }
+</style>
