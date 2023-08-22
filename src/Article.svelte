db320df01c908cc6b9cacaf8b598433fb94fc507
pvilers <pvilers@hotmail.com>
2023-08-22 13:09:24 +0200

    8.0

--- src/Article.svelte
+++ src/Article.svelte
@@ -1,13 +1,15 @@
 <script>
   export let article;
+  import {lazyimage} from 'svelte-lazyimage-cache'
+  const placeholder = 'https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/200.gif'
 </script>
 
 <div class="article">
   <h2>{article.titre}</h2>
-  <img src={article.image} alt={article.titre} width="341" height="237"/>
+  <img alt={article.titre} width="341" height="237" use:lazyimage src={placeholder} data-src={article.image} on:cacheload={()=>console.log('load from cache')} on:imgload={() => console.log("Load from internet")}/>
   <p>{article.texte}</p>
 </div>
-
+<hr />
 <style>
   .article {
     margin: 5rem 0;
--- src/Articles.svelte
+++ src/Articles.svelte
@@ -1,47 +1,13 @@
 <script>
-  import InfiniteScroll from "svelte-infinite-scroll";
-  import articles from './articles.json';
-
-  let page = 0;
-  let size = 5;
-  let datas = [];
-
-  $: datas = [
-     ...datas,
-     ...articles.splice(size * page, size * (page + 1)),
-  ];
+  import articles from "./articles.json";
+  import Article from "./Article.svelte"
 </script>
-
-<h1>Highlights</h1>
 <container>
-<article>
-  {#each datas as article}
- <div class="article">
-  <h2>{article.titre}</h2>
-  <img src={article.image} alt={article.titre} width="341" height="237"/>
-  <p>{article.texte}</p>
-  <h5>
-    All items loaded: {articles.length ? 'No' : 'Yes'}
-  </h5>
-</div>
-    <hr />
-  {/each}
-  <InfiniteScroll
-  hasMore={datas.length < articles.length}
-  threshold={100}
-  on:loadMore={() => page++} />
-</article>
-</container>
+  <article>
+    {#each articles as article}
+      <Article {article} />
+    {/each}
+  </article>
+  </container>
 
-<style>
-  .article {
-    margin: 5rem 0;
-  }
-  img {
-    width: 100%;
-    height: auto;
-    margin: 2rem auto;
-    aspect-ratio: 16 / 10;
-    object-fit: cover;
-  }
-</style>
+
